aaebcce7733f396a3b0445b2d657d49b1cd40e3c
Jake Nickel <jake.a.nickel@gmail.com>
2022-12-28 23:43:17 -0800

    Added weather_display.h

---
+++ weather_display.h
@@ -0,0 +1,7 @@
+/** weather_display.h
+ *  Contains higher level representation of weather panel graphics.
+ *  Supports multiple graphical pages, subwindows, sprites, and 
+ *  smooth scrolling between pages. 
+ *  
+ *  Jake Nickel - 2022
+**/
